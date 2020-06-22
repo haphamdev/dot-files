4e8f0cf1e5601ab1933fbf873d78a2295e98cb60
Ha Pham <ha.pham.ext@personio.de>
2020-06-22 09:25:33 +0700

    Update gco function to remove origin prefix

--- fish/functions/gco.fish
+++ fish/functions/gco.fish
@@ -1,4 +1,4 @@
 function gco
-    git for-each-ref --format='%(refname:short)' | fzf | read -l branch_name
+    git for-each-ref --format='%(refname:short)' | fzf | sed -E 's|origin/||' | read -l branch_name
     git checkout $branch_name
 end
