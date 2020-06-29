c77de3fab93b0982153efde44b3607073a1bdc20
Ha Pham <ha.pham.ext@personio.de>
2020-06-29 11:31:47 +0700

    Update k8s log function to select container

--- fish/functions/klg.fish
+++ fish/functions/klg.fish
@@ -1,5 +1,20 @@
 function klg
+    echo Please select a pod:
     kubectl get pods --template '{{range .items}}{{.metadata.name}}{{"\n"}}{{end}}' |\
-    fzf | read -l pod;
-    kubectl logs -f $pod    
+    fzf --height=15 | read -l pod;
+    echo ✅ $pod is selected
+
+    set -l containers (string split ' ' \
+        (kubectl get pods $pod -o jsonpath='{.spec.containers[*].name}')) # convert string to array
+
+    if test (count $containers) -gt 1 # Multiple containers in pod
+        echo Please select container inside pod $pod:
+        kubectl get pods $pod -o jsonpath='{.spec.containers[*].name}' |\
+        tr " " "\n" | fzf --height=15 | read -l container
+        echo ✅ $container is selected
+
+        kubectl logs -f $pod -c $container
+    else # Only one container in pod
+        kubectl logs -f $pod
+    end
 end
