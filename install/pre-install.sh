a5be8e33f2f6681fc6f731202f94bbfb1a86de06
Ha Pham <ha.pham.ext@personio.de>
2020-07-09 15:11:15 +0700

    Update pre-install list

--- install/pre-install.sh
+++ install/pre-install.sh
@@ -1,14 +1,22 @@
 #!/usr/local/bin/bash
 brew install bash
+brew install htop
+brew install urlview
 brew install fish
+brew install jesseduffield/lazygit/lazygit
 brew install nvim
 brew cask install alacritty
 brew install tmux
 brew install ripgrep
 brew install pgcli
+brew install mycli
 brew install urlview
 brew install htop
 brew install gradle
+brew install jq
+brew install ncdu
+brew install angle-grinder
+
 
 
 # change default shell to fish
