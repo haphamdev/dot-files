f3de55632fab1130f3672961d48955bdf411d7d0
Ha Pham <ha.pham.ext@personio.de>
2020-10-23 16:32:35 +0700

    Remove origin branches from git rebase function

--- fish/functions/grb.fish
+++ fish/functions/grb.fish
@@ -1,8 +1,8 @@
 function grb -d "Rebase current git branch"
     if test (count $argv) -gt 0
-        git for-each-ref --format='%(refname:short)' | fzf --filter $argv[1] | sed -E 's|origin/||' | read branch_name
+        git for-each-ref --format='%(refname:short)' | grep -v origin | fzf --filter $argv[1] | sed -E 's|origin/||' | read branch_name
     else
-        git for-each-ref --format='%(refname:short)' | fzf --height=40% --border | sed -E 's|origin/||' | read branch_name
+        git for-each-ref --format='%(refname:short)' | grep -v origin | fzf --height=40% --border | sed -E 's|origin/||' | read branch_name
     end
 
     echo "Rebasing to branch '$branch_name'"
