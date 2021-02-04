ce7971ecc55fc209564a2659ac80dd7e552d775c
Ha Pham <ha.pham.ext@personio.de>
2021-02-04 08:00:45 +0700

    gdb - Check whether current directory is inside a git repo

--- fish/functions/gdb.fish
+++ fish/functions/gdb.fish
@@ -1,4 +1,11 @@
 function gdb --description "Delete a git branch with fzf"
+    git rev-parse --is-inside-work-tree 2> /dev/null
+    set -l git_error $status
+    if test $git_error -ne 0
+        echo 'Not in git repository'
+        return
+    end
+
     if test (count $argv) -gt 0
         git for-each-ref --format='%(refname:short)' | fzf --filter $argv[1] | sed -E 's|origin/||' | read branch_name
     else
