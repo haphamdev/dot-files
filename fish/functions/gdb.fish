55578c9c68c8b8da55981b9728cfccbb57c3834c
Ha Pham <ha.pham.ext@personio.de>
2021-02-05 09:48:14 +0700

    Remove origin branches from gdb

--- fish/functions/gdb.fish
+++ fish/functions/gdb.fish
@@ -7,9 +7,9 @@ function gdb --description "Delete a git branch with fzf"
     end
 
     if test (count $argv) -gt 0
-        git for-each-ref --format='%(refname:short)' | fzf --filter $argv[1] | sed -E 's|origin/||' | read branch_name
+        git for-each-ref --format='%(refname:short)' | grep -v origin | fzf --filter $argv[1] | sed -E 's|origin/||' | read branch_name
     else
-        git for-each-ref --format='%(refname:short)' | fzf --height=40% --border | sed -E 's|origin/||' | read branch_name
+        git for-each-ref --format='%(refname:short)' | grep -v origin | fzf --height=40% --border | sed -E 's|origin/||' | read branch_name
     end
 
     if test -z "$branch_name"
