41f562e3ff032e02dc40c0303b09f2a4bfdd8f5e
Ha Pham <ha.pham.ext@personio.de>
2021-03-08 14:49:08 +0700

    Update kd to describe k8s pod

--- fish/functions/kd.fish
+++ fish/functions/kd.fish
@@ -1,22 +1,8 @@
 function kd -d "Describe k8s pod"
-    # echo Please select K8s pod:
-    # kubectl get pods --template '{{range .items}}{{.metadata.name}}{{"\n"}}{{end}}' |\
-    # fzf --height=15| read -l pod;
-    # echo ✅ $pod is selected
+    echo Please select K8s pod to describe:
+    kubectl get pods --template '{{range .items}}{{.metadata.name}}{{"\n"}}{{end}}' |\
+    fzf --height=15| read -l pod;
+    echo ✅ $pod is selected
 
-    # set -l container_names_str (kubectl get pods $pod -o jsonpath='{.spec.containers[*].name}')
-    # set -l containers (string split ' ' $container_names_str) # convert string to array
-
-    # if test (count $containers) -gt 1
-    #     echo Please select container inside pod $pod:
-    #     kubectl get pods $pod -o jsonpath='{.spec.containers[*].name}' |\
-    #     tr " " "\n" | fzf --height=15 | read container
-    #     echo ✅ $container is selected
-    # end
-
-    # if set -q container
-    #     kubectl exec -it pods/$pod --container $container -- sh
-    # else
-    #     kubectl exec -it pods/$pod -- sh
-    # end
+    kubectl describe pods/$pod
 end
