12f4a0b02f345a29d7ec8485ba855b96712fbd5d
Ha Pham <ha.pham.ext@personio.de>
2021-03-19 13:58:03 +0700

    Update kdp to support command arguments

--- fish/functions/kdp.fish
+++ fish/functions/kdp.fish
@@ -1,5 +1,20 @@
 function kdp -d "Describe a pod"
-    kubectl get pods --template '{{range .items}}{{.metadata.name}}{{"\n"}}{{end}}' |\
-    fzf | read -l pod;
-    kubectl describe pods/$pod    
+    set -l arg_count (count $argv)
+
+    if test $arg_count -eq 0
+        kubectl get pods --template '{{range .items}}{{.metadata.name}}{{"\n"}}{{end}}' |\
+        fzf --height=15 --border --header="== Select pod:" --reverse | read pod;
+
+        if test -z $pod 
+            echo \ufc38 Aborted
+            return 1
+        end
+    else
+        kubectl get pods --template '{{range .items}}{{.metadata.name}}{{"\n"}}{{end}}' |\
+        fzf --filter $argv[1] | read pod;
+    end
+
+    echo \uf046 Pod: $pod
+
+    kubectl describe pods/$pod | less +G   
 end
