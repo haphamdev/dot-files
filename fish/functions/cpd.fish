7646a27c75761fbb9b870f4c41842543779c68bb
Ha Pham <ha.pham.ext@personio.de>
2021-05-20 08:09:20 +0700

    cpd to copy k8s pod name from all namespaces

--- fish/functions/cpd.fish
+++ fish/functions/cpd.fish
@@ -1,6 +1,6 @@
 function cpd -d "Copy k8s pod name to clipboard"
     echo Please select a pod:
-    kubectl get pods --template '{{range .items}}{{.metadata.name}}{{"\n"}}{{end}}' |\
+    kubectl get pods --template '{{range .items}}{{.metadata.name}}{{"\n"}}{{end}}' --all-namespaces |\
     fzf --height=15 --border | read pod;
     echo $pod | pbcopy
     echo "✅ Pod '$pod' is copied"
