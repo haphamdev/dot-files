cac3d3a31c97c19e87fc2c7b582581457248c66d
Ha Pham <ha.pham.ext@personio.de>
2021-08-04 11:38:40 +0700

    Add description to vim_rg

--- fish/functions/vim_rg.fish
+++ fish/functions/vim_rg.fish
@@ -1,10 +1,10 @@
 function vim_rg -d 'A helper function to use RipGrep in vim with glob supported'
     set argv $argv[2..-1] # Remove -- in argv[1]
-    set -l queries (string split -m 1 '>' "$argv")
+    set -l queries (string split -m 1 '>' "$argv") # split the glob and keyword, separator is >
 
-    if test (count $queries) -eq 1
+    if test (count $queries) -eq 1 # no glob
         rg --column --line-number --no-heading --color=always --smart-case -- $queries[1] || true
-    else if test -z "$queries[1]"
+    else if test -z "$queries[1]" # glob is empty
         rg --column --line-number --no-heading --color=always --smart-case -- $queries[2] || true
     else
         set globs (string replace -a -r '\s+' ' ' $queries[1]\
@@ -14,7 +14,6 @@ function vim_rg -d 'A helper function to use RipGrep in vim with glob supported'
         for g in $globs
             set -a glob_arguments "--iglob '$g'"
         end
-
 
         set command (printf 'rg --column --line-number --no-heading --color=always --smart-case %s -- %s || true' "$glob_arguments" $queries[2])
         eval $command
