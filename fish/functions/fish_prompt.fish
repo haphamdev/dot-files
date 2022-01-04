b9ffdbf46a190fbc52f07077f5895ec6c24bc645
Ha Pham <ha.pham.ext@personio.de>
2022-01-04 09:46:46 +0700

    Remove the apple prefix in fish prompt

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -6,7 +6,6 @@ end
 
 function fish_prompt
   set_color -b 414540
-  set_color -o red; printf '\uf302 '
   set_color -o blue; printf '%s' (whoami)
   set_color $fish_color_autosuggestion[1]; printf '@'
   set_color cyan; printf '%s ' (hostname|cut -d . -f 1)
