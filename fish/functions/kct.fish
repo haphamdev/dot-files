ff76e66611061ca403ad8e30f3d7b3bbadae556a
Ha Pham <ha.pham.ext@personio.de>
2022-01-25 09:29:48 +0700

    Fix kct function

--- fish/functions/kct.fish
+++ fish/functions/kct.fish
@@ -1,10 +1,24 @@
 function kct -d "Switch k8s context"
+    set -l k8s_configs (ls ~/.kube/ | grep yml)
+    set -a k8s_configs local
+
     if test (count $argv) -gt 0
-        ls ~/.kube/ | grep "yml" | fzf --height=10 --filter $argv[1] | read selection
+        printf %s\n $k8s_configs | fzf --height=10 --filter $argv[1] | read selection
     else
-        ls ~/.kube/ | grep "yml" | fzf --height=10 | read selection
+        printf %s\n $k8s_configs | fzf --height=10 | read selection
+    end
+
+    if test -z $selection
+        echo "Aborted or not found"
+        return
+    end
+
+    if test "$selection" = 'local'
+        set -eg KUBECONFIG
+        echo 'Using local k8s context'
+        return
     end
 
     set -xg KUBECONFIG "/Users/hapham/.kube/$selection"
-    echo "Selected k8s context: $KUBECONFIG"
+    echo "Using k8s context: $KUBECONFIG"
 end
