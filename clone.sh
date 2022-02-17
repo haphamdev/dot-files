a60304ea4f57a68e0d03e6eeb61a34ef296afb26
Ha Pham <ha.pham.ext@personio.de>
2022-02-17 22:39:36 +0700

    Update clone.sh to install environment automatically

--- clone.sh
+++ clone.sh
@@ -1,25 +1,27 @@
 #!/bin/sh
 if [ -d $HOME/projects/dot-files ]
 then
-    echo "A dot file directory alreay exists at $HOME/projects/dot-files. Aborted."
-    exit 1
+    echo "A dot file directory alreay exists at $HOME/projects/dot-files. Skipped cloning."
+else
+    echo "Installing git"
+    brew install git
+
+    echo "Generating SSH key"
+    if [ ! -a $HOME/.ssh/id_rsa.pub ]
+    then
+        ssh-keygen -t rsa
+    fi
+
+    pbcopy < $HOME/.ssh/id_rsa.pub
+    echo "SSH public key has been copied. Please add it to GitHub, then press ENTER to continue."
+    read
+
+    # clone the dot-files project
+    echo "Cloning hapham's dot-file project"
+    mkdir $HOME/projects
+    cd $HOME/projects
+    git clone git@github.com:fanliver/dot-files.git
 fi
 
-echo "Installing git"
-brew install git
-
-echo "Generating SSH key"
-if [ ! -a $HOME/.ssh/id_rsa.pub ]
-then
-    ssh-keygen -t rsa
-fi
-
-pbcopy < $HOME/.ssh/id_rsa.pub
-echo "SSH public key has been copied. Please add it to GitHub, then press ENTER to continue."
-read
-
-# clone the dot-files project
-echo "Cloning hapham's dot-file project"
-mkdir $HOME/projects
-cd $HOME/projects
-git clone git@github.com:fanliver/dot-files.git
+./install/pre-install.sh
+./install/install.fish
