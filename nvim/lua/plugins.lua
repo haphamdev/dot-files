439c300d3eb9660a7f96f9c72b71a79f302e4e40
Ha Pham <ha.pham.ext@personio.de>
2022-04-03 09:49:34 +0700

    Indent plugins.lua

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -62,25 +62,25 @@ require('packer').startup(function(use)
 end)
 
 require'nvim-treesitter.configs'.setup {
-  highlight = {
-    enable = true,
-    custom_captures = {
-      -- Highlight the @foo.bar capture group with the "Identifier" highlight group.
-      -- ["foo.bar"] = "Identifier",
+    highlight = {
+        enable = true,
+        custom_captures = {
+        -- Highlight the @foo.bar capture group with the "Identifier" highlight group.
+        -- ["foo.bar"] = "Identifier",
+        },
     },
-  },
-  incremental_selection = {
-    enable = true,
-    keymaps = {
-      init_selection = "gnn",
-      node_incremental = "grn",
-      scope_incremental = "grc",
-      node_decremental = "grm",
+    incremental_selection = {
+        enable = true,
+        keymaps = {
+        init_selection = "gnn",
+        node_incremental = "grn",
+        scope_incremental = "grc",
+        node_decremental = "grm",
+        },
     },
-  },
-  indent = {
-    enable = true
-  }
+    indent = {
+        enable = true
+    },
 }
 
 g.rooter_patterns = { '.git', 'Makefile', '*.sln', 'build/env.sh', 'package.json', 'build.gradle', 'build.gradle.kts' }
