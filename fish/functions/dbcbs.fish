e3ac76057df3d2e794a3953b0011b397ed95a12a
Ha Pham <ha.pham.ext@personio.de>
2023-01-13 08:50:36 +0700

    Add dbsms and dbcbs to access data from SMS and CBS database

---
+++ fish/functions/dbcbs.fish
@@ -0,0 +1,19 @@
+function dbsms
+    if test (count $argv) -gt 0
+        set COMPANY_ID $argv[1]
+        echo "🔑 Company: $COMPANY_ID"
+    else
+        read -P "🔑 Company: " COMPANY_ID
+    end
+
+    print_header "Customer"
+    dbaccess execute-read-query "set personio.company_id to -1; select * from cb_customers where company_id=$COMPANY_ID" --db-identifier=chargebee-service --db-name=chargebee-service
+
+    print_header "Subscription"
+    dbaccess execute-read-query "set personio.company_id to -1; select * from cb_subscriptions where company_id=$COMPANY_ID" --db-identifier=chargebee-service --db-name=chargebee-service
+
+    print_header "Addons"
+    dbaccess execute-read-query "set personio.company_id to -1; select * from cb_subscription_addons inner join cb_addons on cb_subscription_addons.addon_id = cb_addons.id where company_id=$COMPANY_ID" --db-identifier=chargebee-service --db-name=chargebee-service
+end
+
+
---
+++ fish/functions/dbsms.fish
@@ -0,0 +1,20 @@
+function dbsms
+    if test (count $argv) -gt 0
+        set COMPANY_ID $argv[1]
+        echo "🔑 Company: $COMPANY_ID"
+    else
+        read -P "🔑 Company: " COMPANY_ID
+    end
+
+    print_header "Customer"
+    dbaccess execute-read-query "set personio.company_id to -1; select * from customers where company_id=$COMPANY_ID" --db-identifier=subscription-management-service --db-name=subscription-management-service
+
+    print_header "Subscription"
+    dbaccess execute-read-query "set personio.company_id to -1; select * from subscriptions where company_id=$COMPANY_ID" --db-identifier=subscription-management-service --db-name=subscription-management-service
+
+    print_header "Addons"
+    dbaccess execute-read-query "set personio.company_id to -1; select * from subscription_addons inner join addons on subscription_addons.addon_id = addons.id where company_id=$COMPANY_ID" --db-identifier=subscription-management-service --db-name=subscription-management-service
+
+    print_header "Recruiting option"
+    dbaccess execute-read-query "set personio.company_id to -1; select recruiting_options.* from recruiting_options inner join subscriptions on subscriptions.recruiting_option_id = recruiting_options.id where company_id=$COMPANY_ID" --db-identifier=subscription-management-service --db-name=subscription-management-service
+end
