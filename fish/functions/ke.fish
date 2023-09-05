dd0ebf55193effb77dc0de82c6dd5cec4b5f4865
Ha Pham <ha.pham.ext@personio.de>
2023-09-05 16:26:00 +0700

    Update ke, ksh, and klg to use bash script

--- fish/functions/ke.fish
+++ fish/functions/ke.fish
@@ -1,25 +1,3 @@
 function ke -d "Execute a command for a container in a K8s pod"
-    echo Please select K8s pod:
-    kubectl get pods --template '{{range .items}}{{.metadata.name}}{{"\n"}}{{end}}' |\
-    fzf --height=15| read -l pod;
-    echo ✅ $pod is selected
-
-    set -l container_names_str (kubectl get pods $pod -o jsonpath='{.spec.containers[*].name}')
-    set -l containers (string split ' ' $container_names_str) # convert string to array
-
-    if test (count $containers) -gt 1
-        echo Please select container inside pod $pod:
-        kubectl get pods $pod -o jsonpath='{.spec.containers[*].name}' |\
-        tr " " "\n" | fzf --height=15 | read container
-        echo ✅ $container is selected
-    end
-
-    read -P "Enter command to execute: " -l command_string
-    set -l command (echo $command_string | tr ' ' "\n")
-
-    if set -q container
-        kubectl exec -it pods/$pod --container $container -- $command
-    else
-        kubectl exec -it pods/$pod -- $command
-    end
+    ~/projects/klg/ke.sh $argv
 end
