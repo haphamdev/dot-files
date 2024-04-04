6099bac487bddf21d7f3fe32a45cd1ab3ee3c726
Ha Pham <ha.pham.ext@personio.de>
2024-04-04 15:15:39 +0700

    Update nvim-ts-autotag plugin

--- nvim/lua/plugins/nvim-ts-autotag.lua
+++ nvim/lua/plugins/nvim-ts-autotag.lua
@@ -1,6 +1,6 @@
 return {
     "windwp/nvim-ts-autotag",
-    ft = { "php", "typescript", "html", "xml", "markdown", "jsx", "tsx", "javascript" },
+    ft = { "php", "typescript", "html", "xml", "markdown", "jsx", "tsx", "javascript", "typescriptreact" },
     config = function()
         require 'nvim-treesitter.configs'.setup {
             autotag = {
