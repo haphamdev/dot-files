1320d659c546c5b3a6c0b9e6e2e8d293a654685d
Ha Pham <ha.pham.ext@personio.de>
2024-04-10 14:08:28 +0700

    Update switch tmux window script

--- tmux/switch-tmux-window.fish
+++ tmux/switch-tmux-window.fish
@@ -1,5 +1,5 @@
 #!/opt/homebrew/bin/fish
 
 tmux select-window -t (tmux list-window -F "#{window_index} - #{window_name}" \
-    | fzf --reverse --header="Select Tmux Window:" | awk -F- '{print $1}'|  awk '{$1=$1};1')
+    | fzf --reverse --header="Select Tmux Window:"; or exit 0 | awk -F- '{print $1}'|  awk '{$1=$1};1')
 
