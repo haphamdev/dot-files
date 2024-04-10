b83a61b2e6e4f477d39aeef715841821fe472823
Ha Pham <ha.pham.ext@personio.de>
2024-04-10 14:36:02 +0700

    Update switch tmux window script

--- tmux/switch-tmux-window.fish
+++ tmux/switch-tmux-window.fish
@@ -1,5 +1,10 @@
 #!/opt/homebrew/bin/fish
+set SELECTED_WINDOW (tmux list-window -F "#{window_index} - #{window_name}" \
+    | fzf --reverse --header="Select Tmux Window:" | awk -F- '{print $1}'|  awk '{$1=$1};1')
 
-tmux select-window -t (tmux list-window -F "#{window_index} - #{window_name}" \
-    | fzf --reverse --header="Select Tmux Window:"; or exit 0 | awk -F- '{print $1}'|  awk '{$1=$1};1')
+if test -z "$SELECTED_WINDOW"
+    exit 0 #No window was selected, do nothing and exit
+end
+
+tmux select-window -t $SELECTED_WINDOW
 
