182bd804ceeeeb505a6e97aa52dceba5bfd02bef
Ha Pham <ha.pham.ext@personio.de>
2024-05-17 21:47:52 +0700

    Add nvim-surround and autoclose plugins

--- nvim/lua/plugins/others.lua
+++ nvim/lua/plugins/others.lua
@@ -3,4 +3,15 @@ return {
   { "axieax/urlview.nvim" },
   { "norcalli/nvim-colorizer.lua" },
   { "Pocco81/auto-save.nvim" },
+  {
+    "kylechui/nvim-surround",
+    version = "*", -- Use for stability; omit to use `main` branch for the latest features
+    event = "VeryLazy",
+    config = function()
+      require("nvim-surround").setup {
+        -- Configuration here, or leave empty to use defaults
+      }
+    end,
+  },
+  { "m4xshen/autoclose.nvim" },
 }
