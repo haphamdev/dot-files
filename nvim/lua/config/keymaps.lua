332ca6feead3e76ceffb38d093ac5f0c98cc6f41
Ha Pham <ha.pham.ext@personio.de>
2024-11-03 17:31:35 +0700

    Setup LazyVim

---
+++ nvim/lua/config/keymaps.lua
@@ -0,0 +1,61 @@
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
+local function map(mode, lhs, rhs, opts)
+  local options = { remap = false }
+  if opts then
+    options = vim.tbl_extend("force", options, opts)
+  end
+  vim.keymap.set(mode, lhs, rhs, options)
+end
+
+map("i", "jj", "<Esc>", { silent = true, desc = "Exit Insert mode" })
+
+map({ "v", "n" }, "H", "0", { silent = true, desc = "Move to beginning of line" })
+map({ "v", "n" }, "L", "$", { silent = true, desc = "Move to end of line" })
+
+-- <leader> v/h for vertical/horizontal split
+map("n", "<leader>v", ":<C-u>split<CR>")
+map("n", "<leader>h", ":<C-u>vsplit<CR>")
+
+-- Ctrl U in insert mode to switch text case of the current word
+map("i", "<C-u>", "<esc>mqg~iw`qa")
+
+-- <leader>fp to copy the path of current file to clipboard
+map("n", "<leader>fp", [[:let @* = expand('%')<CR>]])
+
+-- x to delete a character without saving it in the " register
+map("n", "x", '"_x', { silent = true })
+
+map("n", "<leader>rl", [[:luafile $MYVIMRC<CR>]])
+
+-- Disable arrow keys
+map("n", "<up>", "<NOP>")
+map("n", "<down>", "<NOP>")
+map("n", "<left>", "<NOP>")
+map("n", "<right>", "<NOP>")
+map("i", "<up>", "<NOP>")
+map("i", "<down>", "<NOP>")
+map("i", "<left>", "<NOP>")
+map("i", "<right>", "<NOP>")
+
+-- smart-splits.nvim
+-- recommended mappings
+-- resizing splits
+-- these keymaps will also accept a range,
+-- for example `10<A-h>` will `resize_left` by `(10 * config.default_amount)`
+map("n", "<A-h>", require("smart-splits").resize_left)
+map("n", "<A-j>", require("smart-splits").resize_down)
+map("n", "<A-k>", require("smart-splits").resize_up)
+map("n", "<A-l>", require("smart-splits").resize_right)
+-- moving between splits
+map("n", "<C-h>", require("smart-splits").move_cursor_left)
+map("n", "<C-j>", require("smart-splits").move_cursor_down)
+map("n", "<C-k>", require("smart-splits").move_cursor_up)
+map("n", "<C-l>", require("smart-splits").move_cursor_right)
+map("n", "<C-\\>", require("smart-splits").move_cursor_previous)
+-- swapping buffers between windows
+map("n", "<leader><leader>h", require("smart-splits").swap_buf_left)
+map("n", "<leader><leader>j", require("smart-splits").swap_buf_down)
+map("n", "<leader><leader>k", require("smart-splits").swap_buf_up)
+map("n", "<leader><leader>l", require("smart-splits").swap_buf_right)
--- nvim/lua/plugins/others.lua
+++ nvim/lua/plugins/others.lua
@@ -1,47 +1,39 @@
 return {
-  { "christoomey/vim-system-copy", event = "BufEnter" },
-  { "axieax/urlview.nvim" },
-  { "norcalli/nvim-colorizer.lua" },
+  { "mrjones2014/smart-splits.nvim" },
   {
-    "kylechui/nvim-surround",
-    version = "*", -- Use for stability; omit to use `main` branch for the latest features
-    event = "VeryLazy",
-    config = function()
-      require("nvim-surround").setup {
-        -- Configuration here, or leave empty to use defaults
-      }
-    end,
-  },
-  { "m4xshen/autoclose.nvim" },
-  { "zdcthomas/yop.nvim" },
-  { "mrjones2014/smart-splits.nvim", opt = {} },
-  { "rest-nvim/rest.nvim" },
-  {
-    "vhyrro/luarocks.nvim",
-    priority = 1000, -- Very high priority is required, luarocks.nvim should run as the first plugin in your config.
-    config = true,
-  },
-  { "VonHeikemen/fine-cmdline.nvim", dependencies = "MunifTanjim/nui.nvim" },
-  {
-    "xiyaowong/transparent.nvim",
+    "folke/tokyonight.nvim",
     opts = {
-      exclude_groups = { "CursorLine", "CursorLineNr" },
-    },
-  },
-  {
-    "stevearc/conform.nvim",
-    enabled = false,
-    opts = {
-      formatters_by_ft = {
-        lua = { "stylua" },
-        -- Conform will run multiple formatters sequentially
-        python = { "isort", "black" },
-        -- You can customize some of the format options for the filetype (:help conform.format)
-        rust = { "rustfmt", lsp_format = "fallback" },
-        -- Conform will run the first available formatter
-        javascript = { "prettierd", "prettier", stop_after_first = true },
+      transparent = true,
+      styles = {
+        sidebars = "transparent",
+        floats = "transparent",
       },
     },
   },
-  { "mrjones2014/smart-splits.nvim" },
+  {
+    "nvim-focus/focus.nvim",
+    version = false,
+    opts = {
+      autoresize = { enable = false },
+      ui = { signcolumn = false },
+    },
+    event = "BufEnter",
+  },
+  {
+    "nvim-zh/colorful-winsep.nvim",
+    config = true,
+    event = { "WinLeave" },
+  },
+  {
+    "folke/tokyonight.nvim",
+    lazy = false,
+    priority = 1000,
+    opts = {
+      on_highlights = function(hl, c)
+        hl.WinSeparator = {
+          fg = "#555555",
+        }
+      end,
+    },
+  },
 }
