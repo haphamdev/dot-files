1cf844ac0706309c404cb84a7ec22e992fd8ea80
Ha Pham <ha.pham.ext@personio.de>
2024-11-15 09:06:20 +0700

    Update fish config

--- fish/config.fish
+++ fish/config.fish
@@ -3,13 +3,14 @@ set -a fish_function_path $HOME/projects/dot-files/fish/functions/hplib
 
 eval (/opt/homebrew/bin/brew shellenv)
 set -x JAVA_HOME (/usr/libexec/java_home -v 17.0.2)
-set -x PATH $JAVA_HOME '/Users/hapham/.config/kotlin-language-server/server/build/install/server/bin' $PATH 
+set -x PATH $JAVA_HOME '/Users/hapham/.config/kotlin-language-server/server/build/install/server/bin' $PATH
 set -x PATH $PATH $HOME/.cargo/bin
 set -x PATH $PATH /usr/local/go/bin
 set -x PATH $PATH (go env GOPATH)/bin
 set -x PATH $PATH '/Users/hapham/Library/Application Support/edgedb/bin'
 set -x PATH $PATH '/usr/hapham/.skdman/bin'
 set -x PATH '/Users/hapham/.asdf/installs/python/3.11.6/bin' $PATH
+set -x PATH $PATH '/Users/hapham/.bun/bin'
 set -x LC_AL en_US.UTF-8
 set -x LANG en_US.UTF-8
 set -x GPG_TTY `tty`
@@ -19,22 +20,22 @@ set -x NVM_DIR "$HOME/.nvm"
 
 # Configure less to clear screen after exit and case-insensitive search
 set -x DELTA_PAGER "/opt/homebrew/bin/less -R -i"
-set -x LESS Ri 
+set -x LESS Ri
 
 set -x DELTA_NAVIGATE 1 # use n/N to navigate file by file in delta
 
 # Personio
-set -x AWS_VAULT_KEYCHAIN_NAME "login"
-set -x AWS_VAULT_KEYCHAIN_NAME "12h"
+set -x AWS_VAULT_KEYCHAIN_NAME login
+set -x AWS_VAULT_KEYCHAIN_NAME 12h
 
 # pnpm
-set -gx PNPM_HOME "/Users/hapham/Library/pnpm"
+set -gx PNPM_HOME /Users/hapham/Library/pnpm
 set -gx PATH "$PNPM_HOME" $PATH
 
 # fish_vi_key_bindings
 
 abbr -a rl 'source ~/.config/fish/config.fish'
-abbr -a x 'exit'
+abbr -a x exit
 abbr -a ef 'nvim ~/projects/dot-files/fish/config.fish'
 abbr -a effn 'nvim ~/.config/fish/functions/'
 abbr -a ea 'nvim ~/.aws'
@@ -60,20 +61,20 @@ abbr -a gl 'git log --oneline --no-merges'
 abbr -a glm 'git log --oneline'
 abbr -a gdf 'git diff'
 abbr -a gdfs 'git diff --staged'
-abbr -a gu 'gitui'
+abbr -a gu gitui
 abbr -a gfu 'git rebase -i HEAD~10'
 abbr -a cbr 'git branch --show-current | pbcopy'
-abbr -a k 'kubectl'
+abbr -a k kubectl
 abbr -a gback 'git checkout -'
 abbr -a gcm 'git commit -m'
 abbr -a gcma 'git commit -am'
 abbr -a gd 'git diff'
 abbr -a batdiff 'git diff --name-only --diff-filter=d | xargs bat --diff'
-abbr -a r 'ranger'
+abbr -a r ranger
 
-abbr -a e 'nvim'
-abbr -a lg 'lazygit'
-abbr -a pc '/opt/homebrew/bin/perctl'
+abbr -a e nvim
+abbr -a lg lazygit
+abbr -a pc /opt/homebrew/bin/perctl
 abbr -a mc '~/projects/personio/monolith-cli'
 abbr -a plg 'lnav ~/projects/personio/app/storage-local/logs/laravel.log'
 abbr -a psn 'cd ~/projects/personio'
@@ -108,11 +109,10 @@ bind -M insert \cn history-search-forward
 
 # In vi mode, jj to escape insert mode
 bind -M insert -m default jj backward-char force-repaint
-    
+
 fish_add_path /usr/local/opt/ruby/bin
 fish_add_path /usr/local/go/bin
 set -x GOPATH $HOME/go
 fish_add_path $GOPATH/bin
 
 zoxide init fish --cmd j | source
-
