19933c0d3e3aaf0cef6d97dafa443f66ca317acd
Ha Pham <ha@repeat.gg>
2025-01-06 08:21:08 +0700

    update fish prompt

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -1,18 +1,16 @@
 # name: Zish
 
-function _is_git_dirty
-  echo (command git status -s --ignore-submodules=dirty 2> /dev/null)
-end
-
 function fish_prompt
-  set_color -b 414540
-  set_color -o blue; printf '%s' (whoami)
-  set_color $fish_color_autosuggestion[1]; printf '@'
-  set_color cyan; printf '%s ' (hostname|cut -d . -f 1)
-  set_color $fish_color_autosuggestion[1]; printf 'in '
-  set_color -o green; printf '%s' (prompt_pwd)
+    set_color -b 414540
+    set_color -o blue
+    printf '%s' (whoami)
+    set_color $fish_color_autosuggestion[1]
+    printf ' in '
+    set_color -o green
+    printf '%s' (prompt_pwd -d 3 -D 3)
 
-  set_color normal
-  set_color yellow; printf ' '
-  set_color normal
+    set_color normal
+    set_color yellow
+    printf ' \n'
+    set_color normal
 end
