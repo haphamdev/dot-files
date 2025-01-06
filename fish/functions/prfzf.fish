af768432261a9feb10c394b6bf84df2d12fcb218
Ha Pham <ha@repeat.gg>
2025-01-06 08:22:01 +0700

    Update prfzf.fish

--- fish/functions/prfzf.fish
+++ fish/functions/prfzf.fish
@@ -3,33 +3,33 @@ function prfzf -d "Go to project in $HOME/projects using fzf search"
 
     # The list of project names and their short-form aliases
     set projects \
-        'admin-panel-service' \
-        'chargebee-cli' \
-        'chargebee-service' \
-        'dot-files' \
-        'company-data-operations-service' \
-        'invoice-service' \
-        'personio' \
-        'subscription-data-monitoring-service' \
-        'subscription-management-service' \
-        'subscription-service' \
-        'subscription-service-client-library'
+        admin-panel-service \
+        chargebee-cli \
+        chargebee-service \
+        dot-files \
+        company-data-operations-service \
+        invoice-service \
+        personio \
+        subscription-data-monitoring-service \
+        subscription-management-service \
+        subscription-service \
+        subscription-service-client-library
 
     set project_aliases \
-        'APS' \
-        'CB-CLI' \
-        'CBS' \
-        'dot' \
-        'CDOS' \
-        'INV' \
-        'Monolith' \
+        APS \
+        CB-CLI \
+        CBS \
+        dot \
+        CDOS \
+        INV \
+        Monolith \
         'Subs Monitor' \
-        'SMS' \
-        'FAS' \
+        SMS \
+        FAS \
         'FAS client lib'
 
     if test (count $argv) -gt 1
-        ls $HOME/projects $HOME/projects/practice | tr " " "\n" | fzf --filter $argv[2] | read result   
+        ls $HOME/projects $HOME/projects/practice | tr " " "\n" | fzf --filter $argv[2] | read result
     else
         ls $HOME/projects $HOME/projects/practice | tr " " "\n" | fzf --border --height=50% | read result
     end
@@ -51,9 +51,9 @@ function prfzf -d "Go to project in $HOME/projects using fzf search"
 
             # rename the current tmux window
             if set -l index (contains -i -- $result $projects)
-                tmux rename-window $project_aliases[$index]
-            else 
-                tmux rename-window $result
+                wezterm cli set-tab-title $project_aliases[$index]
+            else
+                wezterm cli set-tab-title $result
             end
-        end
+    end
 end
