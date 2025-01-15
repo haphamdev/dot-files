83d1a98e7e24bef56bcc03913f62836c33c490f9
Ha Pham <ha@repeat.gg>
2025-01-15 09:44:47 +0700

    Update nvim formatter using conform

---
+++ nvim/lua/plugins/formatter.lua
@@ -0,0 +1,23 @@
+return {
+  {
+    "stevearc/conform.nvim",
+    optional = true,
+    ---@param opts ConformOpts
+    opts = {
+      formatters_by_ft = {
+        javascript = { "prettier" },
+        typescript = { "prettier" },
+        javascriptreact = { "prettier" },
+        typescriptreact = { "prettier" },
+        css = { "prettier" },
+        html = { "prettier" },
+        json = { "prettier" },
+        yaml = { "prettier" },
+        markdown = { "prettier" },
+        graphql = { "prettier" },
+        lua = { "stylua" },
+        python = { "isort", "black" },
+      },
+    },
+  },
+}
