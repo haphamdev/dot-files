56c2c2c9f2641fb6e8a593855bbbf721e12a611a
Ha Pham <ha@repeat.gg>
2025-07-04 07:31:32 +0700

    Update lazyvim

--- nvim/lua/plugins/nvim-treesitter.lua
+++ nvim/lua/plugins/nvim-treesitter.lua
@@ -2,5 +2,14 @@ return {
   "nvim-treesitter/nvim-treesitter",
   opts = {
     ensure_installed = { "prisma" },
+    incremental_selection = {
+      enable = true,
+      keymaps = {
+        init_selection = "<leader><cr>",
+        node_incremental = "<leader><cr>",
+        scope_incremental = false,
+        node_decremental = "<bs>",
+      },
+    },
   },
 }
